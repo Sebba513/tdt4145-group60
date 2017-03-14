9a8c8b83363dc4841e1a1a21b8d7d345587c9040
Sebastian Olafsson <msemse.sebastian@gmail.com>
2017-03-14 15:48:20 +0100

    fixed an error and created a jar file

--- src/Oving2/Connection.java
+++ src/Oving2/Connection.java
@@ -33,50 +33,6 @@ public class Connection {
 
 
 
-
-//            String sql = "SELECT notater FROM TreningsØkt";
-//            ResultSet rs = stmt.executeQuery(sql);
-//
-//            while(rs.next()) {
-//                //Retrieve by column name
-//                String notat = rs.getString("notater");
-//
-//                //Display values
-//                System.out.print("Notat: " + notat);
-//
-//            }
-//            rs.close();
-
-//                String sql = "INSERT INTO TreningsØkt " +
-//                            "VALUES (1,'Første økt', 'Oppstart', '2017-03-10', 'bra luft', 'bra lufting', '2017-03-10 08:15:03', 'det varte lenge', 8, 6, 'dette var en fantastisk økt', 1)";
-//                stmt.executeUpdate(sql);
-
-
-//                String sql = "CREATE TABLE TreningsØkt( " +
-//                        "TreningsøktID        NOT NULL AUTO_INCREMENT," +
-//                        "Navn            varchar(45), " +
-//                        "Dato            date        NOT NULL," +
-//                        "Tidspunkt       datetime    NOT NULL," +
-//                        "Varighet        varchar(45) NOT NULL," +
-//                        "PersonligForm       int check(PersonligForm >=1 and PersonligForm <=10)," +
-//                        "Prestasjon      int check(Prestasjon >=1 and Prestasjon <=10)," +
-//                        "Notater     varchar(1023)," +
-//                        "ResultatID      int, " +
-//                         "PRIMARY KEY(TreningsøktID)," +
-//                        "FOREIGN KEY(ResultatID) REFERENCES Resultat(ResultatID)"
-//);
-
-//                stmt.executeUpdate(sql);
-//                System.out.println("table created");
-
-
-
-//                String sql1 = "DROP TABLE InnendørsØkt";
-//                stmt.executeUpdate(sql1);
-//                System.out.println("table dropped");
-
-
-
         } catch (SQLException ex) {
             System.out.println("Tilkobling feilet: " + ex.getMessage());
         } catch (ClassNotFoundException ex) {
@@ -114,7 +70,7 @@ public class Connection {
         System.out.println("Press 0 for exit ");
 
         while (scanner.hasNext()) {
-            int selection = scanner.nextInt();
+            int selection = Integer.parseInt(scanner.nextLine());
 
             if (selection == 1) {
                 insertOkt(scanner);
@@ -122,10 +78,10 @@ public class Connection {
             if (selection == 2) {
                 getNotes();
             }
-            if (selection == 3) {
+            if (selection == 3){
                 oktCount();
             }
-            if (selection == 0) {
+            if (selection == 0){
                 System.out.println("Exiting. Goodbye! ");
 
                 break;
